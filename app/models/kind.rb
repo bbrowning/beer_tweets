de90fd4a93d155a4a9e4ba6355dd72c7dc2b9978
Ben Browning <bbrownin@redhat.com>
2011-04-25 15:33:01 -0400

    Use db-agnostic column name quoting

--- app/models/kind.rb
+++ app/models/kind.rb
@@ -5,8 +5,9 @@ class Kind < ActiveRecord::Base
                       me my of on or our rt that the to with you your)
 
   def self.top_by_keyword(keywords, max_offset, limit)
-    query = "keyword IN (?) AND \"offset\" IN (?) AND length(word) > 1 AND " +
-      "word NOT IN (?)"
+    offset_column = connection.quote_column_name("offset")
+    query = "keyword IN (?) AND #{offset_column} IN (?) AND length(word) > 1 " +
+      "AND word NOT IN (?)"
 
     results = where(query,keywords, (1..max_offset), EXCLUDED_WORDS).
       group("word").order("count_all DESC").limit(limit).count
